f79e8afdffd0cbdd6d36ee6bf53c99aaf8b078de
Carl Alvarez <carl.alvarez.arg@gmail.com>
2022-05-10 00:38:32 -0300

    errores
    tuve que hacer todo de vuelta por que no levantaba el servidor y sigo sin poder hacer q lo levante, ya no sé q es lo que estoy haciendo mal.

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require ('express');
+
 const app = express ();
-const path = require ('path');
+
+const path = require('path');
+
 const port = 3000;
 
 app.listen(port, () => {console.log ("Habemus Servidore")})
 
-const publicpath = path.join(__dirname, "./public");
+const publicpath = public.join(__dirname, "./public");
 
 app.use(express.static(publicpath));
 
-app.get("/", function (req,res){
+app.get("/", function (req, res){
     res.sendFile (path.resolve(__dirname, "./views/home.html"))
 })
+
