d911456bb2fa9de3efa0d6e2a65bc532fa9156c6
Carl Alvarez <carl.alvarez.arg@gmail.com>
2022-05-10 01:17:47 -0300

    cambio3
    pude levantar el servidor pero no puedo unir el resto de las pagina con con home

--- index.js
+++ index.js
@@ -1,18 +1,45 @@
-const express = require ('express');
+let express = require ('express');
 
-const app = express ();
+let path = require('path');
 
-const path = require('path');
+let app = express ();
 
 const port = 3000;
 
-app.listen(port, () => {console.log ("Habemus Servidore")})
 
-const publicpath = public.join(__dirname, "./public");
-
-app.use(express.static(publicpath));
+app.use(express.static(path.join (__dirname, './public')));
 
 app.get("/", function (req, res){
     res.sendFile (path.resolve(__dirname, "./views/home.html"))
 })
 
+app.listen(port, () => {console.log ("Habemus Servidore")})
+
+
+app.get ("/ofertas", (req, res) => {
+    res.sendFile(path.resolve("./views/ofertas.html"))
+})
+
+app.get ("/tiendas_ofic", (req, res) => {
+    res.sendFile (path.resolve("./views/tiendas_ofic.html"))
+})
+
+app.get ("/vender", (req, res) => {
+    res.sendFile(path.resolve("./views/vender.html"))
+})
+
+app.get ("/ayuda", (req, res) => {
+    res.sendFile(path.resolve("./views/ayuda.html"))
+})
+
+app.get ("/account_creation", (req, res) => {
+    res.sendFile(path.resolve("./views/account_creation.html"))
+})
+
+app.get ("/log_in", (req, res) => {
+    res.sendFile(path.resolve("./views/log_in.html"))
+})
+
+app.get ("/mis_compras", (req, res) => {
+    res.sendFile(path.resolve("./views/mis_compras.html"))
+})
